3bf2c2312254e6bf6e2d20f060dc3b9c02c501fa
Arthur <arthur@neotech.ee>
2016-07-20 12:41:43 +0300

    Add webpack

---
+++ dev-server.js
@@ -0,0 +1,10 @@
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+var wpConfig = require('./webpack.config');
+
+var server = new WebpackDevServer(webpack(wpConfig), {
+    publicPath: wpConfig.output.publicPath,
+    hot: true,
+    historyApiFallback: false
+});
+server.listen(7000);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist/',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    stats: {
+        children: false,
+        assets: false
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!autoprefixer-loader!less?sourceMap')
+            },
+            {
+                test: /\.(png|svg|gif)$/,
+                loader: "url-loader?limit=100000"
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+    ]
+};
