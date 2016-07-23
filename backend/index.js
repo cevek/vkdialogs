5dd9d620b27405e7f6da305b25b670870b1339b4
cody <cevek@ya.ru>
2016-07-23 14:23:34 +0500

    Add backend serve statics

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const koa = require('koa');
 const app = koa();
 const kc = require('koa-controller');
 const env = process.env.NODE_ENV || 'development';
+const serve = require('koa-static');
 const config = require(`./config/config.${env}.js`);
 
-
 app.config = config;
+app.use(serve('../frontend/dist/'));
 app.use(kc.router());
 app.models = require('./app/models')(app);
 
