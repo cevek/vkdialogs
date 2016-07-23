07d8164f048acb311fafc615762168ee3471b928
cody <cevek@ya.ru>
2016-07-23 21:35:21 +0500

    Add set names utf8

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 module.exports = function (app) {
     const pool = mysql.createPool(app.config.db);
     const Users = require('./users');
-    return {
+    const models = {
         query: function (query, params) {
             return new Promise((resolve, reject) => {
                 pool.query(query, params, (err, result) => err ? reject(err) : resolve(result));
@@ -13,4 +13,6 @@ module.exports = function (app) {
         },
         users: new Users(app),
     };
+    models.query("SET NAMES 'utf8'");
+    return models;
 };
