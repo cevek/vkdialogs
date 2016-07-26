382661d998a398f50141a802c56d785308c77a53
cody <cevek@ya.ru>
2016-07-26 17:10:14 +0500

    Fix validate search query

--- backend/app/models/users.js
+++ backend/app/models/users.js
@@ -5,17 +5,21 @@ module.exports = class Users {
         this.app = app;
     }
 
-    //todo: make unlimit
     *findAll() {
-        return yield this.app.models.query('SELECT id, first_name, last_name, photo FROM users LIMIT 1000');
+        return yield this.app.models.query('SELECT id, first_name, last_name, photo FROM users');
     }
 
     *findByQuery(query) {
         const value = (Array.isArray(query) ? query : [query]).slice(0, 10)
-            .map(q =>
-                q.substr(0, 30).replace(/^[\wа-яё ]]/ig, '') + '*').join(' ');
+            .map(q => {
+                const result = q.substr(0, 30).replace(/[^\wа-яё ._\-]/ig, '').trim();
+                return result ? result + '*' : '';
+            }).join(' ').trim();
 
-        return yield this.app.models.query(`SELECT id, first_name, last_name, photo FROM users WHERE 
-                MATCH(first_name, last_name, nickname) AGAINST (? IN BOOLEAN MODE) LIMIT 100`, value);
+        const results = value
+            ? yield this.app.models.query(`SELECT id FROM users WHERE  MATCH(first_name, last_name, nickname) AGAINST (? IN BOOLEAN MODE)`, value)
+            : [];
+
+        return results.map(user => user.id);
     }
 };
