61ab308038b435d57ccfab2ccfccc6ed19cd6445
cody <cevek@ya.ru>
2016-07-26 17:29:05 +0500

    Add gzip

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ const kc = require('koa-controller');
 const env = process.env.NODE_ENV || 'development';
 const serve = require('koa-static');
 const config = require(`./config/config.${env}.js`);
+const gzip = require('koa-gzip');
 
 app.config = config;
+app.use(gzip());
 app.use(serve('../frontend/dist/'));
 app.use(kc.router());
 app.models = require('./app/models')(app);
